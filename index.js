cacdec1adc87a19eda62d8c3c70f81a5aff4e629
faroke <faroke-wb@outlook.fr>
2018-06-05 23:56:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ bot.on("ready", function () {
 	console.log("Membres : " + bot.users.size)
 });
 
-
+// Demonstration basique d'une intéraction utilisateur/Bot
 bot.on('message', msg => {
 
   if (msg.content === 'Ping') {
     msg.reply('Pong :ping_pong: ');
   }
-
-
 });
+
+// Ici on envoie un message lorsqu'un Nouveau membre entre sur le serveur, le message de bienvenue est envoyé en message privé
 bot.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
     return channel.send('Bienvenue sur mon serveur ' + member.displayName)
@@ -36,29 +36,27 @@ bot.on('guildMemberAdd', member => {
 
 
 bot.on("message", function(message) {
+    // Lorsqu'un message pouvant être intercepté par le Bot est un message qu'il a lui même écrit, alors on ne s'en occupe pas
     if (message.author.equals(bot.user)) return;
-
+    //Si un message ne commence pas par le préfix attribué (soit "!"), alors on ne s'en occupe pas
     if (!message.content.startsWith(PREFIX)) return;
 
+    // On définit la variable args qui sera égal au message commençent par le prefix, chaque mot correspondra à une valeur de 0 à K
     var args = message.content.substring(PREFIX.length).split (" ");
 
-    var args2 = message.content.split(" ").slice(1);
 
-    var suffix = args2.join(" ");
-
-    var reason = args.slice(1).join(" ");
-
+    // Raccourcis
     var user = message.mentions.users.first();
-    
+    // Raccourcis
     var guild = message.guild;
-    
+    // Raccourcis
     var member = message.member;
-    
+    // On définit que modlog est une variable qui correspond à un salon discord qui à pour nom mod-log
     var modlog = member.guild.channels.find("name", "mod-log")
 
-
+    //Switch va nous permettre d'interragir à chaque fois que quelqu'un utilisera une commande, donc un message contenant en première argument (args[0]) le prefix, chaque demande sera traiter en minuscule, si quelqu'un ecrit !PURGE, le bot traitera !purge
     switch (args[0].toLowerCase()) {
-            
+            //Simple affichage de texte
             case "help":
             var embed = new Discord.RichEmbed()
             .addField("!ban", "Cette commande permet de bannir un utilisateur ! Pour l'utiliser, faites !ban @(utilisateur)")
@@ -66,6 +64,7 @@ bot.on("message", function(message) {
                 .addField("!ping ou Ping", "Joue au ping pong avec le chatbot")
                 .addField("!github", "Obtenir le code source de notre ChatBot")
                 .addField("!purge", "Supprimer un nombre de message à la suite ! Pour l'utiliser, faites !purge nombredemessage")
+                .addField("!question", "Poser une question au ChatBot! Pour l'utiliser, faite !question 'ma question'")
                 .setColor("#01A9DB")
                 .setAuthor(message.author.username, message.author.avatarURL)
                 .setDescription("Voici les commandes du bot !")
@@ -74,11 +73,12 @@ bot.on("message", function(message) {
                 message.channel.sendEmbed(embed);
             break;
         case "kick":
+            // gestion de droit d'administration
             if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.sendMessage("Erreur, permission invalide");
             if(!modlog) return message.reply("Je ne trouve pas de channel mod-log.");
             if (message.mentions.users.size < 1) return message.reply("Erreur Argument")
             message.guild.member(user).kick();
-
+// Systeme de log
             var embed = new Discord.RichEmbed()
             .addField("Commande :", "KICK")
             .addField("Utilisateur :", user.username)
@@ -107,7 +107,7 @@ bot.on("message", function(message) {
             member.guild.channels.find("name", "mod-log").sendEmbed(embed);
             message.delete();
             break;
-      case "comment":
+      case "question":
       message.reply("https://www.lmgtfy.com/?q=" + args[1]);
       break;
       /*
@@ -154,4 +154,4 @@ bot.on("message", function(message) {
     }
 });
 
-bot.login(TOKEN)
+bot.login('NDE1ODcyMDYxNDA0MjE3MzU1.DfgNVg.CaosrIzxEiOtVBnSr7PPoqZiV0Y')
